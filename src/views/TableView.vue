399cfd8c7b3aac14de86b0b5d91e0fd2d2f14938
Mai Pham <maibach2022@gmail.com>
2026-01-27 16:06:02 +0100

    add total row to the table

--- src/views/TableView.vue
+++ src/views/TableView.vue
@@ -1,119 +1,144 @@
-<script lang="ts">
-import { defineComponent } from 'vue'
+<script setup lang="ts">
+import { computed, onMounted, ref } from 'vue'
 import DataTable from 'primevue/datatable'
 import Column from 'primevue/column'
+import ColumnGroup from 'primevue/columngroup'
+import { Row } from 'primevue'
 
-export default defineComponent({
-  name: 'TableView',
-  components: {
-    DataTable,
-    Column,
-  },
-  setup() {
-    const fruits = [
-      {
-        id: '1',
-        name: 'Apple',
-        category: 'Pome',
-        color: 'Red',
-        calories: 52,
-        sugar: 10.4,
-        price: 1.29,
-      },
-      {
-        id: '2',
-        name: 'Banana',
-        category: 'Berry',
-        color: 'Yellow',
-        calories: 89,
-        sugar: 12.2,
-        price: 0.49,
-      },
-      {
-        id: '3',
-        name: 'Orange',
-        category: 'Citrus',
-        color: 'Orange',
-        calories: 47,
-        sugar: 9.4,
-        price: 0.99,
-      },
-      {
-        id: '4',
-        name: 'Strawberry',
-        category: 'Berry',
-        color: 'Red',
-        calories: 33,
-        sugar: 4.9,
-        price: 2.99,
-      },
-      {
-        id: '5',
-        name: 'Blueberry',
-        category: 'Berry',
-        color: 'Blue',
-        calories: 57,
-        sugar: 9.7,
-        price: 3.99,
-      },
-      {
-        id: '6',
-        name: 'Mango',
-        category: 'Drupe',
-        color: 'Yellow',
-        calories: 60,
-        sugar: 13.7,
-        price: 1.99,
-      },
-      {
-        id: '7',
-        name: 'Pineapple',
-        category: 'Multiple',
-        color: 'Yellow',
-        calories: 50,
-        sugar: 9.9,
-        price: 2.49,
-      },
-      {
-        id: '8',
-        name: 'Watermelon',
-        category: 'Pepo',
-        color: 'Green/Red',
-        calories: 30,
-        sugar: 6.2,
-        price: 3.99,
-      },
-      {
-        id: '9',
-        name: 'Kiwi',
-        category: 'Berry',
-        color: 'Brown/Green',
-        calories: 61,
-        sugar: 9.0,
-        price: 1.49,
-      },
-      {
-        id: '10',
-        name: 'Grape',
-        category: 'Berry',
-        color: 'Purple',
-        calories: 69,
-        sugar: 16.1,
-        price: 2.99,
-      },
-    ]
+interface Fruit {
+  id: string
+  name: string
+  category: string
+  color: string
+  calories: number
+  sugar: number
+  price: number
+}
 
-    return {
-      fruits,
-    }
+type Totals = Pick<Fruit, 'calories' | 'sugar' | 'price' >
+
+const fruits : Fruit[] = [
+  {
+    id: '1',
+    name: 'Apple',
+    category: 'Pome',
+    color: 'Red',
+    calories: 52,
+    sugar: 10.4,
+    price: 1.29,
   },
+  {
+    id: '2',
+    name: 'Banana',
+    category: 'Berry',
+    color: 'Yellow',
+    calories: 89,
+    sugar: 12.2,
+    price: 0.49,
+  },
+  {
+    id: '3',
+    name: 'Orange',
+    category: 'Citrus',
+    color: 'Orange',
+    calories: 47,
+    sugar: 9.4,
+    price: 0.99,
+  },
+  {
+    id: '4',
+    name: 'Strawberry',
+    category: 'Berry',
+    color: 'Red',
+    calories: 33,
+    sugar: 4.9,
+    price: 2.99,
+  },
+  {
+    id: '5',
+    name: 'Blueberry',
+    category: 'Berry',
+    color: 'Blue',
+    calories: 57,
+    sugar: 9.7,
+    price: 3.99,
+  },
+  {
+    id: '6',
+    name: 'Mango',
+    category: 'Drupe',
+    color: 'Yellow',
+    calories: 60,
+    sugar: 13.7,
+    price: 1.99,
+  },
+  {
+    id: '7',
+    name: 'Pineapple',
+    category: 'Multiple',
+    color: 'Yellow',
+    calories: 50,
+    sugar: 9.9,
+    price: 2.49,
+  },
+  {
+    id: '8',
+    name: 'Watermelon',
+    category: 'Pepo',
+    color: 'Green/Red',
+    calories: 30,
+    sugar: 6.2,
+    price: 3.99,
+  },
+  {
+    id: '9',
+    name: 'Kiwi',
+    category: 'Berry',
+    color: 'Brown/Green',
+    calories: 61,
+    sugar: 9.0,
+    price: 1.49,
+  },
+  {
+    id: '10',
+    name: 'Grape',
+    category: 'Berry',
+    color: 'Purple',
+    calories: 69,
+    sugar: 16.1,
+    price: 2.99,
+  },
+]
+
+const dt = ref()
+onMounted(() => {
+  console.log(dt.value)
+})
+
+const pageTotals = computed(() => {
+  if (!dt.value) {
+    return { calories: 0, sugar: 0, price: 0 }
+  }
+  const allProcessed = dt.value.processedData
+  const first = dt.value.d_first
+  const rows = dt.value.d_rows
+  const currentDisplayed = allProcessed.slice(first, first + rows)
+  return currentDisplayed.reduce(
+    (acc: Totals, item: Fruit) => {
+      acc.calories += Number(item.calories || 0)
+      acc.sugar += Number(item.sugar || 0)
+      acc.price += Number(item.price || 0)
+      return acc
+    },
+    { calories: 0, sugar: 0, price: 0 } as Totals,
+  )
 })
 </script>
 
 <template>
   <div class="home">
     <h1>Fruits in a table</h1>
-    <DataTable :value="fruits" stripedRows paginator :rows="5">
+    <DataTable ref="dt" :value="fruits" stripedRows paginator :rows="5">
       <Column field="id" header="ID"></Column>
       <Column field="name" header="Name"></Column>
       <Column field="category" header="Category"></Column>
@@ -127,6 +152,14 @@ export default defineComponent({
       <Column field="price" header="Price">
         <template #body="slotProps"> ${{ slotProps.data.price.toFixed(2) }} </template>
       </Column>
+      <ColumnGroup type="footer">
+        <Row>
+          <Column footer="Page Total:" :colspan="4" footerStyle="text-align:right" />
+          <Column :footer="pageTotals.calories + ' kcal'" />
+          <Column :footer="pageTotals.sugar.toFixed(1) + 'g'" />
+          <Column :footer="'$' + pageTotals.price.toFixed(2)" />
+        </Row>
+      </ColumnGroup>
     </DataTable>
   </div>
 </template>
